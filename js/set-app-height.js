174cdaac0fc907356cb6e12d69fda2096dc31028
EXARTeo <teofask2004@gmail.com>
2025-08-01 15:33:32 +0300

    loading

--- js/set-app-height.js
+++ js/set-app-height.js
@@ -3,8 +3,10 @@
   const setAppHeight = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.body.classList.remove('loading');
   };
 
   // Εκτελείται μόλις φορτωθεί το DOM (όχι μετά εικόνες, fonts κλπ)
+  document.body.classList.add('loading');
   document.addEventListener('DOMContentLoaded', setAppHeight);
 })();
