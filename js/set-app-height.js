7fb482fc7ff9bc202dcacc67b0b5767df2aa4377
EXARTeo <teofask2004@gmail.com>
2025-08-05 14:51:45 +0300

    test all local

--- js/set-app-height.js
+++ js/set-app-height.js
@@ -1,12 +1,26 @@
-// sets --vh custom property on page load
 (() => {
+  const isMobile = /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i.test(navigator.userAgent);
+
   const setAppHeight = () => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    // Μόνο στο load αφαιρούμε το loading
     document.body.classList.remove('loading');
+    document.body.classList.add('loaded');
   };
 
-  // Εκτελείται μόλις φορτωθεί το DOM (όχι μετά εικόνες, fonts κλπ)
-  document.body.classList.add('loading');
-  document.addEventListener('DOMContentLoaded', setAppHeight);
+  // Αν δεν έχει την loading, βάλε την (backup)
+  if (!document.body.classList.contains('loading')) {
+    document.body.classList.add('loading');
+  }
+
+  if (isMobile) {
+    // Μόνο στην αρχή και όταν αλλάζει προσανατολισμός (rotation)
+    window.addEventListener('load', setAppHeight);
+    window.addEventListener('orientationchange', setAppHeight);
+  } else {
+    // Σε desktop κάνε update σε κάθε resize
+    window.addEventListener('load', setAppHeight);
+    window.addEventListener('resize', setAppHeight);
+  }
 })();
